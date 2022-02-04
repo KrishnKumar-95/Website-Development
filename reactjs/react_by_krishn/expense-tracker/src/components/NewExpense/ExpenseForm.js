fe24a3aaa0f3292fa27646c35a000be49b8b8ae9
krishan kumar <krishnkumar180895@gmail.com>
2022-02-04 12:13:29 +0530

    Now we can fill the expense details in the expense form and expense data will be logged in console and set back to its original state

--- reactjs/react_by_krishn/expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ reactjs/react_by_krishn/expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -43,6 +43,17 @@ const ExpenseForm = ()=>{
         // This prevent the default behaviour of sending request to the hosting server
         // But we want to handle it using the JavaScript manually
         event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+       
+        console.log(expenseData);
+        setenteredTitle("");
+        setenteredAmount("");
+        setenteredDate("");
     }
 
     return (
@@ -51,13 +62,15 @@ const ExpenseForm = ()=>{
                 <div className="new-expense__control">
                     <label>Title</label>
                     <input 
-                        type="text" 
+                        type="text"
+                        value={enteredTitle}
                         onChange={(e)=>{titleChangeHandler(e)}} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
                     <input 
-                        type="number" 
+                        type="number"
+                        value={enteredAmount}
                         onChange={(e)=>{amountChangeHandler(e)}} 
                         min="0.01" 
                         step="0.01"/>
@@ -66,7 +79,8 @@ const ExpenseForm = ()=>{
                     <label>Date</label>
                     <input 
                         type="date" 
-                        onChange={(e)=>{dateChangeHandler(e)}} 
+                        onChange={(e)=>{dateChangeHandler(e)}}
+                        value={enteredDate}
                         min="2022-01-01" 
                         max="2023-01-01"/>
                 </div>
